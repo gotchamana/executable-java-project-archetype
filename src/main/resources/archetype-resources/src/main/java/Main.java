2bbc35f5e0e0c005295131bd24af77248acb4fef
shootingstar <jinhung.c@gmail.com>
2021-10-23 23:50:46 +0800

    Init repo

---
+++ src/main/resources/archetype-resources/src/main/java/Main.java
@@ -0,0 +1,14 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
